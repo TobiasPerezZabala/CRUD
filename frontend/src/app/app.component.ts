8a3594ad8b02ce306b9f553d8d58f40fc3b390df
Franco-Spinelli <spinellifranco04@gmail.com>
2024-10-15 12:54:01 -0300

    change front components

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet, RouterModule } from '@angular/router';
+import { PersonaService } from './persona.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    CommonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit{
+  personas: any[] = [];
+
+  constructor(private personaService: PersonaService) { }
+
+
+  ngOnInit(): void {
+    this.personaService.getPersonas().subscribe(data => {
+      this.personas = data;
+      console.log(this.personas);
+    });
+  }
 }
